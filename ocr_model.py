10be98cef294e86fce77249b2934913c40b4175f
Ironside <93154332+motakosay@users.noreply.github.com>
2025-06-14 05:25:42 +0300

    Update ocr_model.py

--- ocr_model.py
+++ ocr_model.py
@@ -30,5 +30,5 @@ def text_to_speech(arabic_text):
     #ipd.Audio(audio_path, autoplay=True)
 """
 
-ahmed = image_to_text("/content/YSI.png")
+ahmed = image_to_text("/content/arTest.jpg")
 print(ahmed)
