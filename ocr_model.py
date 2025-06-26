5cf9eb55bfc04b421bb70569d600f3d28b30c9f0
Ironside <93154332+motakosay@users.noreply.github.com>
2025-06-27 01:45:59 +0300

    Update ocr_model.py

--- ocr_model.py
+++ ocr_model.py
@@ -1,3 +1,4 @@
+import sys
 import matplotlib.pyplot as plt
 import pytesseract
 from gtts import gTTS
@@ -5,16 +6,6 @@ import IPython.display as ipd
 from requests import get  # to make GET request
 import cv2
 from playsound import playsound
-"""
-def download(url, file_name):
-    # open in binary mode
-    with open(file_name, "wb") as file:
-        # get request
-        response = get(url)
-        # write to file
-        file.write(response.content)
-"""
-#download("https://github.com/tesseract-ocr/tessdata/raw/main/ara.traineddata","ara.traineddata")
 
 
 def image_to_text(filename):
@@ -22,13 +13,11 @@ def image_to_text(filename):
     arabic_text=pytesseract.image_to_string(img_cv , lang='ara',config= ".")
     return arabic_text
 
-"""
-def text_to_speech(arabic_text):
-    tts = gTTS(arabic_text, lang='ar')
-    tts.save('test.mp3')
 
-    #ipd.Audio(audio_path, autoplay=True)
-"""
-
-ahmed = image_to_text("/content/arTest.jpg")
-print(ahmed)
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python ocr_model.py <image_path>")
+    else:
+        image_path = sys.argv[1]
+        ahmed = image_to_text(image_path)
+        print(ahmed)
