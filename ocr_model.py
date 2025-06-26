f3ce0a72b14e4c11f55160c547d58567f27f9182
Ironside <93154332+motakosay@users.noreply.github.com>
2025-06-27 01:50:10 +0300

    Update ocr_model.py

--- ocr_model.py
+++ ocr_model.py
@@ -1,18 +1,11 @@
 import sys
-import matplotlib.pyplot as plt
 import pytesseract
-from gtts import gTTS
-import IPython.display as ipd
-from requests import get  # to make GET request
 import cv2
-from playsound import playsound
-
 
 def image_to_text(filename):
     img_cv = cv2.imread(filename)
-    arabic_text=pytesseract.image_to_string(img_cv , lang='ara',config= ".")
+    arabic_text = pytesseract.image_to_string(img_cv, lang='ara', config=".")
     return arabic_text
-
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
